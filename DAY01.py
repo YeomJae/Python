9f29c33e447c8b73579b180a0ca161031bc7c2a1
YeomJae <baeaek@gmail.com>
2024-10-09 15:05:28 +0900

    DAY01_complete

--- DAY01.py
+++ DAY01.py
@@ -1,12 +1,15 @@
-sales = 100,000,000
-name = input(“이름: ”)
-time = input(“출근시간(HHMM): ”)
-if(int(time) <= 900)
+sales = 100000000
+name = input("1. 이름을 입력하세요: ")
+time = input("2. 출근시간을 입력하세요(HHMM): ")
+if(int(time) <= 900):
   sales = int(sales) * 1.3
-  print(str(name) + “님 ” + str(time)+ “ 출근 ” + “- ” + str(sales))
-elif(int(time) <= 905)
+  print(str(name) + "님 " + str(time)+ " 출근 - ", end="")
+  print("{:,}".format(int(sales)))
+elif(int(time) <= 905):
   sales = int(sales) * 1.1
-  print(str(name) + “님 ” + str(time)+ “ 출근인정” + “- ” + str(sales))
-else(int(time) > 905)
+  print(str(name) + "님 " + str(time)+ " 출근인정 - ", end="")
+  print("{:,}".format(int(sales)))
+else:
   sales = int(sales) * 0.85
-  print(str(name) + “님 ” + str(time) + “ 지각” + “- ” + str(sales))
+  print(str(name) + "님 " + str(time) + " 지각 - ", end="")
+  print("{:,}".format(int(sales)))
