8b6610338d6f4a613144b3aef1946848f213343b
YeomJae <baeaek@gmail.com>
2024-10-13 15:24:35 +0900

    DAY01_2nd

--- DAY01.py
+++ DAY01.py
@@ -1,3 +1,5 @@
+# First
+"""
 sales = 100000000
 name = input("1. 이름을 입력하세요: ")
 time = input("2. 출근시간을 입력하세요(HHMM): ")
@@ -13,3 +15,19 @@ else:
   sales = int(sales) * 0.85
   print(str(name) + "님 " + str(time) + " 지각 - ", end="")
   print("{:,}".format(int(sales)))
+  """
+
+# Second
+sales = 100000000
+name = input("1. 이름을 입력하세요: ")
+time = input("2. 출근시간을 입력하세요(HHMM): ")
+hour = int(time[:2])
+min = int(time[2:])
+hour_to_min = hour * 60
+
+if(hour_to_min + min <= 540):
+  print(f"{name}님 {time} 출근 - {"{:,}".format(int(sales * 1.3))}")
+elif(hour_to_min + min <= 545):
+  print(f"{name}님 {time} 출근인정 - {"{:,}".format(int(sales * 1.1))}")
+else:
+  print(f"{name}님 {time} 지각 - {"{:,}".format(int(sales * 0.85))}")
